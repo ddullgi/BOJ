6c901a52d6b218742861cee2cd169fbcbe54c8aa
shrewslampe <97648143+shrewslampe@users.noreply.github.com>
2022-01-29 02:17:26 +0900

    [Silver V] Title: 쉽게 푸는 문제, Time: 68 ms, Memory: 30860 KB -BaekjoonHub

--- 백준/Silver/1292.쉽게 푸는 문제/쉽게 푸는 문제.py
+++ 백준/Silver/1292.쉽게 푸는 문제/쉽게 푸는 문제.py
@@ -1,17 +1,28 @@
 A, B = map(int, input().split())
-
 cnt = 0
 num_list = []
-n = 0
 
-while cnt < B:
-    n += 1
-    for i in range(n):
-        num_list += [n]
+N = 0
+K = 0
+
+while K < A:
+    N += 1
+    K = N * (N + 1) /2
+
+k = int(K) - N
+
+while True:
+    for i in range(N):
+        num_list += [N]
         cnt += 1
+        if cnt == B - k:
+            break
+    if cnt == B - k:
+        break
+    N += 1
 
 result = 0
-for j in range(A-1, B):
+for j in range(A - k-1, B-k):
     result += num_list[j]
 
 print(result)
