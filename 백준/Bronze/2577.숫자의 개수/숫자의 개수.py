3fb5ae24e65a80ebc18b026a0f679a9baec0b567
shrewslampe <97648143+shrewslampe@users.noreply.github.com>
2022-01-30 02:59:09 +0900

    [Bronze II] Title: 숫자의 개수, Time: 68 ms, Memory: 30860 KB -BaekjoonHub

---
+++ 백준/Bronze/2577.숫자의 개수/숫자의 개수.py
@@ -0,0 +1,13 @@
+A = int(input())
+B = int(input())
+C = int(input())
+
+R = A * B * C
+
+
+for i in range(10):
+    cnt = 0
+    for j in str(R):
+        if j == str(i):
+            cnt += 1
+    print(cnt)
