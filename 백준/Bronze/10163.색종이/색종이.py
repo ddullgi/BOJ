994c2eea04c708f48101bcb09100c873e63676e7
yun <97648143+shrewslampe@users.noreply.github.com>
2022-02-27 20:26:23 +0900

    [Bronze I] Title: 색종이, Time: 720 ms, Memory: 133172 KB -BaekjoonHub

---
+++ 백준/Bronze/10163.색종이/색종이.py
@@ -0,0 +1,14 @@
+area = [[0]*1001 for _ in range(1001)]
+num = [0] * 101
+
+N = int(input())
+for n in range(1, N+1):
+    x1, y1, x2, y2 = map(int, input().split())
+    num[n] = x2 * y2
+    for i in range(y1, y1+y2):
+        for j in range(x1, x1+x2):
+            num[area[i][j]] -= 1
+            area[i][j] = n
+
+for n in range(1, N+1):
+    print(num[n])
