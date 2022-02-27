ecd7fc0adfc7482453e6a5cecf75e0d2c04848a3
yun <97648143+shrewslampe@users.noreply.github.com>
2022-02-27 22:39:31 +0900

    [Bronze I] Title: 색종이, Time: 712 ms, Memory: 133036 KB -BaekjoonHub

--- 백준/Bronze/10163.색종이/색종이.py
+++ 백준/Bronze/10163.색종이/색종이.py
@@ -1,10 +1,10 @@
-import sys; input = sys.stdin.readline
+import sys
 area = [[0]*1001 for _ in range(1001)]
 num = [0] * 101
 
 N = int(input())
 for n in range(1, N+1):
-    x1, y1, x2, y2 = map(int, input().split())
+    x1, y1, x2, y2 = map(int,sys.stdin.readline().split())
     num[n] = x2 * y2
     for i in range(y1, y1+y2):
         for j in range(x1, x1+x2):
