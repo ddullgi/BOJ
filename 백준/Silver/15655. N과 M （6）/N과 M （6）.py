f9414fdeeedbc7aeeceda68599f619c9afb5e58f
yun <97648143+shrewslampe@users.noreply.github.com>
2022-03-15 02:55:33 +0900

    [Silver III] Title: N과 M (6), Time: 68 ms, Memory: 30864 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.py
@@ -0,0 +1,23 @@
+N, M = map(int, input().split())
+arr = list(map(int, input().split()))
+arr.sort()
+num = [0] * N
+stack = []
+
+def NM(ar, lt, nn):
+    if len(lt) == M:
+        print(*lt)
+        return
+    for i in range(0, N):
+        if nn[i]:
+            continue
+        else:
+            if len(lt) > 0 and ar[i] < lt[-1]:
+                continue
+            lt.append(ar[i])
+            num[i] = 1
+            NM(ar, lt, nn)
+            num[i] = 0
+            lt.pop()
+
+NM(arr, stack, num)
