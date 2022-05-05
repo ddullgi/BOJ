ecc95b30e48cc021dc970ae7d45358fdc1ed5530
yun <97648143+shrewslampe@users.noreply.github.com>
2022-05-05 20:52:30 +0900

    [Gold IV] Title: 특정한 최단 경로, Time: 464 ms, Memory: 133944 KB -BaekjoonHub

--- 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.py
+++ 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.py
@@ -2,6 +2,7 @@ from heapq import heappop, heappush
 
 def find_root(start, end):
     D = [0xffffff] * (N + 1)
+
     visit = [False] * (N + 1)
     Q = [(0, start)]
     D[start] = 0
@@ -15,7 +16,7 @@ def find_root(start, end):
             if not visit[v] and D[v] > D[u] + w:
                 D[v] = D[u] + w
                 heappush(Q,(D[v], v))
-    return D[end]
+    return D
 
 
 N, E = map(int, input().split())
@@ -30,11 +31,12 @@ else:
         G[v].append((u, w))
 
 v1, v2 = map(int, input().split())
+XXX = find_root(1, v1)
+XXX2 = find_root(N, v1)
+v1v2 = find_root(v1, v2)[v2]
 
-v1v2 = find_root(v1, v2)
-
-result1 = find_root(1, v1) + v1v2 + find_root(v2, N)
-result2 = find_root(1, v2) + v1v2 + find_root(v1, N)
+result1 = XXX[v1] + v1v2 + XXX2[v2]
+result2 = XXX[v2] + v1v2 + XXX2[v1]
 
 result = min(result1, result2)
 if result >= 0xffffff:
