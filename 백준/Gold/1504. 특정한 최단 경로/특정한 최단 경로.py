05eded05a68979b340ff5c7a7293597a7a0426e9
yun <97648143+shrewslampe@users.noreply.github.com>
2022-05-05 20:42:38 +0900

    [Gold IV] Title: 특정한 최단 경로, Time: 492 ms, Memory: 134808 KB -BaekjoonHub

--- 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.py
+++ 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.py
@@ -2,7 +2,6 @@ from heapq import heappop, heappush
 
 def find_root(start, end):
     D = [0xffffff] * (N + 1)
-    P = [i for i in range(N + 1)]
     visit = [False] * (N + 1)
     Q = [(0, start)]
     D[start] = 0
@@ -15,7 +14,6 @@ def find_root(start, end):
         for v, w in G[u]:
             if not visit[v] and D[v] > D[u] + w:
                 D[v] = D[u] + w
-                P[v] = u
                 heappush(Q,(D[v], v))
     return D[end]
 
@@ -34,30 +32,6 @@ else:
 v1, v2 = map(int, input().split())
 
 v1v2 = find_root(v1, v2)
-# SV1, PV1 = find_root(1, v1)
-# SV2, PV2 = find_root(1, v2)
-
-# n1 = v1
-# while True:
-#     if PV1[n1] == v2:
-#         result1 = find_root(1, v1)[0] + find_root(v1, N)[0]
-#         break
-#     elif PV1[n1] == 1:
-#         result1 = find_root(1, v1)[0] + v1v2 + find_root(v2, N)[0]
-#         break
-#     else:
-#         n1 = PV1[n1]
-#
-# n2 = v2
-# while True:
-#     if PV2[n2] == v1:
-#         result2 = find_root(1, v2)[0] + find_root(v2, N)[0]
-#         break
-#     elif PV2[n2] == 1:
-#         result2 = find_root(1, v2)[0] + v1v2 + find_root(v1, N)[0]
-#         break
-#     else:
-#         n2 = PV2[n2]
 
 result1 = find_root(1, v1) + v1v2 + find_root(v2, N)
 result2 = find_root(1, v2) + v1v2 + find_root(v1, N)
