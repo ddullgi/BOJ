dcd21d4a587734802a43594102b2c4d80d17e8d8
yun <97648143+shrewslampe@users.noreply.github.com>
2022-07-26 18:42:28 +0900

    [level 2] Title: 튜플, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/64065. 튜플/튜플.py
+++ 프로그래머스/lv2/64065. 튜플/튜플.py
@@ -2,15 +2,7 @@ def solution(s):
     lis = s.lstrip("{")
     lis = lis.rstrip("}")
     string = "aa"
-    lis2 = []
-    for i in range(2, len(lis)):
-        if lis[i - 2:i + 1] == "},{":
-            lis2.append(string[2:])
-            string=""
-        else:
-            string += lis[i-2]
-    string += lis[-2:]
-    lis2.append(string[2:])
+    lis2 = list(lis.split("},{"))
     lis2.sort(key=len)
     lis3 = []
     for j in lis2:
