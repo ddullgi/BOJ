d37bd9e554681bb1b8d0ee6048b66b60befc4d48
yun <97648143+shrewslampe@users.noreply.github.com>
2022-08-03 02:48:36 +0900

    [Silver V] Title: 생일, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/5635. 생일/생일.py
@@ -0,0 +1,10 @@
+n = int(input())
+arr = []
+for _ in range(n):
+    name, day, month, year = input().split()
+    arr.append((name, int(day), int(month), int(year)))
+
+arr.sort(key=lambda x: (x[3], x[2], x[1]))
+
+print(arr[-1][0])
+print(arr[0][0])
