4b4eb6889660bb911a4ad215f2433c1ba3afbbb1
yun <97648143+ddullgi@users.noreply.github.com>
2023-03-29 01:13:47 +0900

    [level 2] Title: N-Queen, Time: 6891.49 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/12952. N－Queen/N－Queen.py
+++ 프로그래머스/lv2/12952. N－Queen/N－Queen.py
@@ -1,26 +1,80 @@
-def possible(x, y, n, col):
-    for i in range(x):
-        if y == col[i] or abs(y - col[i]) == x - i:
-            return False
-    return True
+result = 0
 
 
-def queen(x, n, col):
-    if x == n:
-        return 1
-    count = 0
+def nqueen(board, n ,col):
+    global result
     
-    for y in range(n):
-        if possible(x, y, n, col):
-            col[x] = y
-            count += queen(x + 1, n, col)
+    if col == n:
+        result += 1
+        return
     
-    return count
-
+    for row in range(n):
+        board[col] = row
+        
+        for x in range(col):
+            # 열이 같으면 돌아
+            if board[x] == row:
+                break
+            # 대각선이 겹치면 돌아가
+            if abs(board[x] - row) == col - x:
+                break
+        # 중복이 없으면 더 들어가
+        else: 
+            nqueen(board, n, col + 1)
+    
+            
+    
 
 def solution(n):
+    Q = [0] * n
+    nqueen(Q, n, 0)
+    return result
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def nqueen(queen, n, row):
+#     count = 0
     
-    col = [0] * n
-    answer = queen(0, n, col)
+#     if n == row:
+#         return 1
     
-    return answer
+#     # 가로로 한번만 진행
+#     for col in range(n):
+#         queen[row] = col
+        
+#         # 앞의 것과 비교
+#         for x in range(row):
+#             if queen[x] == queen[row]:
+#                 break
+#             if abs(queen[x]-queen[row]) == row-x:
+#                 break
+#         else:
+#             count += nqueen(queen, n, row+1)
+#     return count
+
+# def solution(n):
+#     queen = [0] * n
+#     return nqueen(queen, n, 0)
--- 프로그래머스/lv2/12952. N－Queen/README.md
+++ 프로그래머스/lv2/12952. N－Queen/README.md
@@ -1,10 +1,10 @@
 # [level 2] N-Queen - 12952 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12952?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12952) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 7510.17 ms
+메모리: 10.1 MB, 시간: 6891.49 ms
 
 ### 구분
 
