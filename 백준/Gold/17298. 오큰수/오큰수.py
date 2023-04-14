8ec246fa756576501640bb70cf17396c2569a306
yun <97648143+ddullgi@users.noreply.github.com>
2023-04-14 23:04:11 +0900

    [Gold IV] Title: 오큰수, Time: 1128 ms, Memory: 155576 KB -BaekjoonHub

---
+++ 백준/Gold/17298. 오큰수/오큰수.py
@@ -0,0 +1,13 @@
+N = int(input())
+A = list(map(int, input().split()))
+
+answer = [-1] * N
+stack = []
+
+for i in range(N):
+    while stack and A[stack[-1]] < A[i]:
+        answer[stack.pop()] = A[i]
+    stack.append(i)
+
+
+print(*answer)
