6efd4749ee27539b888022296e422b557dc34ca3
yun <97648143+ddullgi@users.noreply.github.com>
2023-06-20 15:49:42 +0900

    [unrated] Title: 바탕화면 정리, Time: 0.18 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/161990. 바탕화면 정리/바탕화면 정리.py
@@ -0,0 +1,10 @@
+def solution(wallpaper):
+    r = []
+    c = []
+    for i in range(len(wallpaper)):
+        for j in range(len(wallpaper[0])):
+            if wallpaper[i][j] == "#":
+                r.append(i)
+                c.append(j)
+
+    return [min(r), min(c), max(r) + 1, max(c) + 1]
