70e0cc815eea246f6344277a7e57ede181d5abf6
yun <97648143+ddullgi@users.noreply.github.com>
2023-07-14 19:10:36 +0900

    [Gold III] Title: A[j]-A[i]+A[l]-A[k], Time: 500 ms, Memory: 268460 KB -BaekjoonHub

--- 백준/Gold/15487. A［j］－A［i］＋A［l］－A［k］/A［j］－A［i］＋A［l］－A［k］.py
+++ 백준/Gold/15487. A［j］－A［i］＋A［l］－A［k］/A［j］－A［i］＋A［l］－A［k］.py
@@ -7,12 +7,10 @@ bdp[-1] = (arr[-1] - arr[-2], max(arr[-2], arr[-1]))
 for i in range(2, N - 2):
     fdp.append((max(fdp[-1][0], arr[i] - fdp[-1][1]), min(fdp[-1][1], arr[i])))
 
+answer = fdp[N - 4][0] + bdp[N - 4][0]
+
 for j in range(N - 5, -1, -1):
     bdp[j] = (max(bdp[j + 1][0], bdp[j + 1][1] - arr[j + 2]), max(arr[j + 2], bdp[j + 1][1]))
-
-answer = -0xffffffffff
-
-for k in range(N - 3):
-    answer = max(answer, fdp[k][0] + bdp[k][0])
+    answer = max(answer, fdp[j][0] + bdp[j][0])
 
 print(answer)
