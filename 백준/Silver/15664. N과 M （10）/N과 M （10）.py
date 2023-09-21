fa0067f8dec5a8613cf00afc2c784e24538e5493
yun <97648143+ddullgi@users.noreply.github.com>
2023-09-21 22:42:30 +0900

    [Silver II] Title: N과 M (10), Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.py
@@ -0,0 +1,23 @@
+N, M = map(int, input().split())
+nums = sorted(list(map(int, input().split())))
+flag = [False] * N
+temp = []
+
+def back():
+    if len(temp) == M:
+        print(*temp)
+        return
+    check = 0
+
+    for i in range(N):
+        if not flag[i] and check != nums[i]:
+            if temp and temp[-1] > nums[i]:
+                continue
+            flag[i] = True
+            temp.append(nums[i])
+            check = nums[i]
+            back()
+            flag[i] = False
+            temp.pop()
+
+back()
