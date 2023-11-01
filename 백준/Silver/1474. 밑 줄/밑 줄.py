edf270b61af1feb7ea0bee971719b6899edf9562
yun <97648143+ddullgi@users.noreply.github.com>
2023-11-01 23:43:28 +0900

    [Silver I] Title: 밑 줄, Time: 40 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Silver/1474. 밑 줄/밑 줄.py
@@ -0,0 +1,15 @@
+n, m = map(int, input().split())
+data = [input() for _ in range(n)]
+default_len, r = divmod(m - sum(map(len, data)), n - 1)
+result = data[0]
+
+for idx in range(1, n):
+    if data[idx][0].islower() and r != 0:
+        r -= 1
+        result += '_' * (default_len + 1) + data[idx]
+    elif idx + r == n:
+        r -= 1
+        result += '_' * (default_len + 1) + data[idx]
+    else:
+        result += '_' * default_len + data[idx]
+print(result)
