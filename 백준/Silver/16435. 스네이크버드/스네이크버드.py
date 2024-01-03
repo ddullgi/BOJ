19f4b6f9d7d0251014f96954a12c31a6ed1cee33
yun <97648143+ddullgi@users.noreply.github.com>
2024-01-03 23:58:47 +0900

    [Silver V] Title: 스네이크버드, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/16435. 스네이크버드/스네이크버드.py
@@ -0,0 +1,11 @@
+N, L = map(int, input().split())
+h = list(map(int, input().split()))
+h.sort()
+
+for fruit in h:
+    if fruit > L:
+        break
+    elif fruit <= L:
+        L += 1
+
+print(L)
