91215bacf683abca4b0afd5b4787dbc0e6d61a46
yun <97648143+ddullgi@users.noreply.github.com>
2024-02-07 23:15:02 +0900

    [Silver III] Title: 블로그, Time: 176 ms, Memory: 58780 KB -BaekjoonHub

---
+++ 백준/Silver/21921. 블로그/블로그.py
@@ -0,0 +1,21 @@
+n, x = list(map(int, input().split()))
+nums = list(map(int, input().split()))
+
+res = sum(nums[:x])
+temp = res
+days = 1
+
+for i in range(x, n):
+    temp -= nums[i - x]
+    temp += nums[i]
+    if res < temp:
+        res = temp
+        days = 1
+    elif res == temp:
+        days += 1
+
+if res > 0:
+    print(res)
+    print(days)
+else:
+    print("SAD")
