7fd0adf4e786fbda5ffd3a68ec9afb0e65850fc6
SangYun Hwang <s_infinite@naver.com>
2024-11-12 01:06:45 +0900

    [level 2] Title: 중성화 여부 파악하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
+++ 프로그래머스/2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
@@ -5,7 +5,7 @@ SELECT
     CASE
         WHEN SEX_UPON_INTAKE LIKE 'Neutered%' THEN 'O'
         WHEN SEX_UPON_INTAKE LIKE 'Spayed%' THEN 'O'
-        WHEN SEX_UPON_INTAKE LIKE 'Intact%' THEN 'X'
+        ELSE 'X'
     END AS 중성화
 FROM ANIMAL_INS
 ORDER BY ANIMAL_ID
